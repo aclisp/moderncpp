8faf19494cef972e0ffe097fe3992a4b29e01985
aclisp <aclisp@gmail.com>
2014-08-20 22:45:35 +0800

    initial

---
+++ sync2a/src/scratch.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : scratch.cpp
+// Author      : Homer Huang
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int foo(int a)
+{
+	a += 1;
+	cout << "I'm in foo" << endl;
+	return a;
+}
+
+int main()
+{
+	int a = 1;
+	int b = foo(a);
+
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
